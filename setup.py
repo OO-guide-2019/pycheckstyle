e692a616bb0dcdbce93cc663fde7b615cfce3dfa
HansBug <killog@126.com>
2018-09-01 13:54:24 +0800

    fix setup package

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
+_package_name = "pycheckstyle"
+_version = "0.3.1"
 setup(
-    name="pycheckstyle",
-    version="0.3.1",
-    packages=[
-        "pycheckstyle"
-    ]
+    name=_package_name,
+    version=_version,
+    packages=find_packages(
+        include=(_package_name, "%s.*" % _package_name)
+    )
 )
